c1e5ef40f17a640aa14674f577cf7476775e9288
Pusparaj04 <pusparajkarna143@gmail.com>
2024-06-19 18:02:21 +0545

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# Eligibility-Check-for-Voting
+ This code can check whether you're eligible to vote or not on the basis of information provided by you. It is a simple code written in C
---
+++ continueStatement.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+int main()
+{
+    // continue statement:
+    int i, age;
+    int PY, YOB;
+    printf("Enter the present year: ");
+    scanf("%d", &PY);
+
+    for (int i = 1; i < 100; i++)
+    {
+        printf("%d) ", i);
+        printf("Enter the year of birth: ");
+        scanf("%d", &YOB);
+        age = PY - YOB;
+        printf("Since you're %d years old, ", age);
+        
+
+        if (age > 0 && age < 18)
+        {
+            printf("You are not eligible to vote.\n");
+            continue;
+        } else if(age<0){
+            printf("Invalid age.\n \a");
+        }
+        else
+        {
+            break;
+        }
+    }
+    printf("You're eligible to vote!!");
+    return 0;
+}
